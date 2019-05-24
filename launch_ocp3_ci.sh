b05e89742f839b923049df2512b0099277034dca
Erik Nelson <erik@nsk.io>
2019-05-24 15:35:54 -0400

    Add oa helper scripts

---
+++ launch_ocp3_ci.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+if [[ "$AWS_DEFAULT_REGION" == "" ]]; then
+  echo 'Must have AWS_DEFAULT_REGION defined in environment'
+  exit 1
+fi
+
+MIG_CI=/git/mig/mig-ci
+export WORKSPACE=/git/mig/mig-ci-workspace
+export EC2_REGION="$AWS_DEFAULT_REGION"
+export EC2_KEY='libra'
+# Will default to 3.11, but can be set with this var
+#export OPENSHIFT_VERSION='v3.11'
+
+pushd $MIG_CI
+ansible-playbook get_ocp3_oa.yml
+ansible-playbook deploy_ocp3_cluster.yml
+popd
