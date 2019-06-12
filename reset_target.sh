6e97f1ecaa6eeb7c1784051f96326ac8a58ae0f6
Erik Nelson <erik@nsk.io>
2019-06-12 13:29:34 -0400

    Improve reset_target

--- reset_target.sh
+++ reset_target.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
-oc delete project mysql-persistent && oc delete pv pv9 && oc delete -n mig migmigrations --all && oc delete -n mig migplans --all
+# NOTE: Will clean a target cluster, usually the ocp4 cluster.
+# Optionally you could remove the line that deletes the mig plans if you
+# just want to reuse the same plan (you should be able to do that).
+
+# Kill the migrated app
+oc delete project mysql-persistent
+
+# Clean the PV that was claimed
+DELETEPV=$(o get pv | grep mysql-persistent | awk '{ print $1 }')
+oc delete pv $DELETEPV
+
+# Clean migrations
+oc delete mig migmigrations --all
+
+# Optionally kill any of the plans
+oc delete -n mig migplans --all
