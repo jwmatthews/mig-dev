12d76ff7a2897082ca09ed26303075d8ef8e2957
Erik Nelson <erik@nsk.io>
2019-08-08 08:25:10 -0400

    Add cluster-loader

---
+++ cluster-loader/deploy_cluster_loader.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+TESTS_IMG_TAG=latest
+#TESTS_IMG_TAG="4.1"
+
+podman run \
+  -v ${KUBECONFIG}:/root/.kube/config \
+  -v ${_dir}:/tmp/cluster-loader \
+  -i "quay.io/openshift/origin-tests:${TESTS_IMG_TAG}" \
+  /bin/bash -c 'export KUBECONFIG=/root/.kube/config && VIPERCONFIG=/tmp/cluster-loader/nsk-test.yaml openshift-tests run-test "[Feature:Performance][Serial][Slow] Load cluster concurrently with templates [Suite:openshift]"'
